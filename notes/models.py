d536983d75b6b506393597e7ed14bdf2a1f4590a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-30 06:28:20 -0400

    feat(notes): create a '__str__' method to view printed objects

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,10 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     
+    def __str__(self):
+        self.id = id
+        self.title = title
+        self.content = content
+        
+        return (f'id = {id}\ntitle = {title}\ncontent = {content}\n')
+    
