45dfd948396dca413aa006d12d2984342011f366
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-30 09:10:52 -0400

    feat(models): run 'python manage.py makemigrations' > '1' > 'timezone.now'

--- notes/models.py
+++ notes/models.py
@@ -5,6 +5,8 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
         
     def __repr__(self):
         return(f"\nid = {self.id}\ntitle = {self.title}\ncontent = {self.content}")
