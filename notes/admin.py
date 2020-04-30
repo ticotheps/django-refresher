481d64ddce5f2528114114cf92fcdf7e1478c18a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-30 09:28:09 -0400

    feat(admin.py): import the 'PersonalNote' class and register it with the admin portal

--- notes/admin.py
+++ notes/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Tells our admin portal which tables we're interested in seeing
 #   at http://localhost:8000/admin
 from .models import Note
+from .models import PersonalNote
 
 # Allows read-only fields to show up in admin portal
 class NoteAdmin(admin.ModelAdmin):
@@ -10,3 +11,6 @@ class NoteAdmin(admin.ModelAdmin):
 
 # Registers the 'Note' model with the admin portal
 admin.site.register(Note, NoteAdmin)
+
+# Registers the 'PersonalNote' model with the admin portal
+admin.site.register(PersonalNote)
